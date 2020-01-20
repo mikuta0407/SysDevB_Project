824b39e1b2503788493f857a5388d77e4b546a82
mikuta0407 <flute.takkun0407@gmail.com>
2020-01-21 00:52:24 +0900

    コメント大量追加とラジオボタン無効化足りなかったとこ直した

--- PresentationTimer/app/src/main/java/com/mikuta0407/presentation_timer/MainActivity.java
+++ PresentationTimer/app/src/main/java/com/mikuta0407/presentation_timer/MainActivity.java
@@ -35,8 +35,8 @@ public class MainActivity extends AppCompatActivity {
 	boolean paused = false; //一時停止状態の記録
 	int mode = 1; //1: 発表 2: 質問 3: 点滅用
 	boolean flashmode = true;   //点滅の反転。本当はflashpartsに書くべきなのだが、回転対策のためここに記載。
-	boolean alerm = true;
-	boolean rang = false;
+	boolean alerm = true; //とりあえずデフォルトでTrueなので。これは状態復元時にスイッチの状態を維持するため
+	boolean rang = false;   //残り1分の音が鳴ったか鳴らないか
 
 	boolean recovered = false; // 再生成かどうかの判定
 
@@ -45,9 +45,9 @@ public class MainActivity extends AppCompatActivity {
 	private ProgressBar timeProgressBar; //プログレスバー
 	private FloatingActionButton start_pause;   //スタートストップボタン
 	private FloatingActionButton cancel;    //キャンセルボタン
-	private Switch alermSwitch;
-	private CountDownTimer countDown; //カウントダウンクラス
-	private CountDownTimer flashCountDown;
+	private Switch alermSwitch; //音のオンオフスイッチ
+	private CountDownTimer countDown; //カウントダウンクラス(ノーマルタイマー)
+	private CountDownTimer flashCountDown; //点滅用タイマー
 	private SimpleDateFormat dataFormat = new SimpleDateFormat("mm:ss.S", java.util.Locale.JAPANESE); //データフォーマット
 	private RadioGroup ptime_radiobox;  //発表時間設定用ラジオボタン
 	private RadioGroup qtime_radiobox;  //質問時間設定用ラジオボタン
@@ -60,7 +60,7 @@ public class MainActivity extends AppCompatActivity {
 
 
 	//public long ptime = 20000; //発表時間設定を記録 //ptime = 600000; //一度も動作させたことがないときにActivityが再生成されるとptimeが何もなくて虚無になるので
-	public long ptime;
+	public long ptime; //発表時間設定を記録
 	public long qtime; //質問時間設定を記録
 	private long leftTime; //残り時間を記録
 	private long flashTime; //点滅用(回転対策にここに記載)
@@ -114,7 +114,7 @@ public class MainActivity extends AppCompatActivity {
 				.setMaxStreams(3)
 				.build();
 
-		// ファイル準備
+		// 音声ファイル準備
 		lastOneMinSound = soundPool.load(this, R.raw.last_one_min_sound, 1);
 		ptimeEndSound = soundPool.load(this, R.raw.ptime_end_sound, 1);
 		qtimeEndSound = soundPool.load(this, R.raw.qtime_end_sound, 1);
@@ -141,7 +141,7 @@ public class MainActivity extends AppCompatActivity {
 			leftTime = savedInstanceState.getLong("leftTimestatus");
 			flashTime = savedInstanceState.getLong("flashTimestatus");
 
-			//UI系
+			/* UI系 */
 
 			//色復元
 			if (mode == 1){
@@ -173,10 +173,11 @@ public class MainActivity extends AppCompatActivity {
 				if (paused) { //一時停止だった場合
 					//数字復元
 					updateCountDownText(); //一時停止時に回転した場合の文字復元
+					radioEnabledFalse(); //ラジオボタン無効化
 				}
 
 				if (finished) { //停止中だった場合
-					recovered = false;
+					recovered = false; //このあと特に必要ないのでfalse
 					pastText.setText("00:00.0"); //除算の余りの関係で00:00.1になるので
 
 					//停止時の文字復元
@@ -193,7 +194,7 @@ public class MainActivity extends AppCompatActivity {
 			}
 
 
-			// ラジオボタン
+			/* ラジオボタン */
 			// 発表時間
 			//if (ptime == 600000) {
 			if (ptime == 20000) { //デモ用
@@ -213,7 +214,9 @@ public class MainActivity extends AppCompatActivity {
 		}
 
 
-		// 以下メイン処理
+		/* 以下メイン処理 */
+
+		// アラームスイッチの状態を一度確保
 		alerm = alermSwitch.isChecked();
 
 		// スタート・ストップボタンが押されたら
@@ -221,18 +224,20 @@ public class MainActivity extends AppCompatActivity {
 			@Override
 			public void onClick(View spClick) {
 				if (run) { //動作中なら
-					if (mode == 1 || mode == 2){
+					if (mode == 1 || mode == 2){ //普通のタイマー状態だったら
 						start_pause.setImageDrawable(getResources().getDrawable(R.drawable.ic_play)); //再生ボタンに変更
-						pauseTimer();
-					} else if (mode == 3 || mode == 4){
+						pauseTimer(); //一時停止に移行
+					} else if (mode == 3 || mode == 4){ //点滅中だったら
 						Log.i("デバッグ","点滅中なので何もしませんよ");
 					}
-				} else {        // 動いてなかったら
+				} else {    // 動いてなかったら
 					start_pause.setImageDrawable(getResources().getDrawable(R.drawable.ic_pause)); //一時停止ボタンに変更
 
-					if (finished == true){ //完了後または初期状態の場合
+					if (finished == true){ //完了後または初期状態の場合(これからタイマーを実行)
+
 						// 発表時間設定の選択を取得
 						int ptimeId = ptime_radiobox.getCheckedRadioButtonId();
+
 						// ptimeに時間設定。(ms)
 						if (ptimeId == R.id.ptime10) {
 							//ptime = 600000;  //10*60*1000 ms
@@ -245,8 +250,8 @@ public class MainActivity extends AppCompatActivity {
 
 
 						// 質問時間設定の選択を取得
-
 						int qtimeId = qtime_radiobox.getCheckedRadioButtonId();
+
 						// qtimeに時間設定。(ms)
 						if (qtimeId == R.id.qtime5) {
 							//qtime = 300000;  //5*60*1000 ms
@@ -256,8 +261,8 @@ public class MainActivity extends AppCompatActivity {
 						}
 
 					}
-					radioEnabledFalse();
-					Log.i("デバッグ", "ラジオボタンを無効化します(ボタン押したとき)");
+
+					radioEnabledFalse(); //ラジオボタン無効化
 					startTimer(); // タイマースタート
 				}
 			}
@@ -267,37 +272,32 @@ public class MainActivity extends AppCompatActivity {
 		cancel.setOnClickListener(new View.OnClickListener(){
 			@Override
 			public void onClick(View v){
-				resetTimer();
-				start_pause.setImageDrawable(getResources().getDrawable(R.drawable.ic_play)); //再生ボタンに変更
-				ptime_radiobox.setEnabled(true);
-				//String resetFormatted = String.format(java.util.Locale.JAPANESE, "%02d:%02d.%01d", 0, 0, 0);
-				//timerText.setText(resetFormatted);
-				//pastText.setText(resetFormatted);
+				resetTimer(); //リセット叩くよ
 			}
 		});
+
 	}
 
 	private void startTimer() {  // タイマー実行
 
-		Log.i("デバッグ", "startTimerを叩きました。startTimer内でのptimeは "+ ptime);
-		Log.i("デバッグ", "startTimerを叩きました。startTimer内でのmodeは "+ mode);
-		run = true;
+		run = true; //動作中
+
 		if (paused == true){ //一時停止復帰
 			paused = false;
-			Log.i("デバッグ", "一時停止から復帰しましたわよ");
+			Log.i("デバッグ", "startTimerは一時停止復帰モードで起動しました");
 		} else if (recovered){
 			recovered = false;
-			Log.i("デバッグ", "startTimerは回転修正モードで起動しました");
+			Log.i("デバッグ", "startTimerはActivity再生成モードで起動しました");
 		} else { // ノーマル起動
 			Log.i("デバッグ", "startTimerはノーマルモードで起動しました");
-			if (mode == 1) {
-				leftTime = ptime;
-				Log.i("デバッグ", "leftTimeに値を代入しました。ptimeは"+ ptime +"、leftTimeは" + leftTime);
-			} else if (mode == 2) {
-				leftTime = qtime;
+			if (mode == 1) {    //発表モードなら
+				leftTime = ptime; //残り時間用変数に発表時間の値を代入
+			} else if (mode == 2) { //質問モードなら
+				leftTime = qtime; //残り時間用変数に質問時間の値を代入
 			}
 		}
 
+		/* タイマー起動 */
 		Log.i("デバッグ", "countDownTimerを起動します。leftTimeは "+ leftTime);
 
 		countDown = new CountDownTimer(leftTime,10) {
@@ -306,146 +306,175 @@ public class MainActivity extends AppCompatActivity {
 				leftTime = millisUntilFinished;
 				//if (leftTime <= 60000 && rang == false){
 				if (leftTime <= 10000 && rang == false){ //デモ用
-					rang = true;
-					if (alermSwitch.isChecked()) {
-						soundPool.play(lastOneMinSound, 1f, 1f, 0, 0, 1.0f);
+					rang = true; //音がなったか鳴らないかにかかわらずとりあえず鳴るタイミングになったので(複数回鳴ることを防止)
+
+					if (alermSwitch.isChecked()) { //アラーム音スイッチがONなら
+						soundPool.play(lastOneMinSound, 1f, 1f, 0, 0, 1.0f); //ﾋﾟﾋﾟﾋﾟﾋﾟ
 					}
 				}
-				updateCountDownText();
+
+				updateCountDownText(); //時間表示更新 (10ms毎にね)
 			}
 
 			@Override
-			public void onFinish() {
+			public void onFinish() { //終わったら
 				if (mode == 1) { //まだ質問に入ってなかったら(発表が終わったら)
 					Log.i("デバッグ", "発表時間が終わりました。");
+
+					//音を鳴らすぜ!
 					if (alermSwitch.isChecked()) {soundPool.play(ptimeEndSound, 1f, 1f, 0, 0, 1.0f);}
-					leftTime = qtime;
-					updateCountDownText();
+
+					leftTime = qtime;   //次のために質問時間用の時間をセット
+
+					updateCountDownText();  //おまじない
+
 				} else { //質問も終わったら
 					Log.i("デバッグ", "質問時間も終わりました");
+
+					//音を鳴らすぜ!
 					if (alermSwitch.isChecked()) { soundPool.play(qtimeEndSound, 1f, 1f, 0, 1, 1.0f);}
 				}
-				rang = false;
-				flashTime = 5000;
-				flash();
+
+				rang = false; //複数回鳴るのを防止
+				flashTime = 5000; //5秒間点滅してもらいます
+				flash();    //それでは点滅どうぞー
 			}
 		}.start();
 	}
 
 
-
-	private void pauseTimer(){  // 一時停止
-		countDown.cancel();
-		paused = true;
-		run = false;
-		finished = false;
+	//一時停止
+	private void pauseTimer(){
+		countDown.cancel(); //一旦キャンセルします
+		paused = true; //一時停止状態に
+		run = false;    //動作はしてない
+		finished = false;   //終了もしてない
 	}
 
-	private void resetTimer(){  // リセット
-		if (run || paused) {
-			if (mode == 1 || mode == 2){
-				countDown.cancel();
-			}
-			if (mode == 3 || mode == 4){
-				flashCountDown.cancel();
+	//リセット
+	private void resetTimer(){
+		if (run || paused) { //動作中か一時停止中のみ効きます(ぬるぽ防止)
+			if (mode == 1 || mode == 2){ //普通のタイマーだったら
+				countDown.cancel(); //キャンセル
+			} else  if (mode == 3 || mode == 4){ //点滅モードだったら
+				flashCountDown.cancel();    //点滅モードもキャンセル
 			}
 
-			leftTime = ptime;
-			updateCountDownText();
-			finished = true;
-			paused = false;
-			mode = 1;
-			run = false;
-			radioEnabledTrue();
-			timeProgressBar.setProgress(100);
-			flashTime = 5000;
+			leftTime = ptime;   //残り時間用変数をptimeに戻す
+			updateCountDownText();  //表記をリセット
+			finished = true;    //終了状態
+			paused = false;     //一時停止状態ではない
+			mode = 1;           //初期状態に
+			run = false;        //動作中じゃない
+			radioEnabledTrue(); //ラジオボタンを有効化
+			timeProgressBar.setProgress(100);   //円を白くする
+			flashTime = 5000;      //点滅が途中キャンセルされてる可能性もあるのでね･･･おまじない的な
+
+			//ボタンとバーの色を青に戻す
 			start_pause.setBackgroundTintList(ColorStateList.valueOf(getResources().getColor(R.color.tcu)));
 			timeProgressBar.setBackgroundTintList(ColorStateList.valueOf(getResources().getColor(R.color.tcu)));
+
 			start_pause.setImageDrawable(getResources().getDrawable(R.drawable.ic_play)); //再生ボタンに変更
-			pastText.setText("00:00.0");
+
+			pastText.setText("00:00.0");    //文字を0に戻す(言語関係ないのでセーフ)
 		}
 	}
 
-	private void updateCountDownText(){ // 時刻の表示
-		int minutes = (int)(leftTime/1000)/60;
-		int seconds = (int)(leftTime/1000)%60;
-		int ms = (int)((leftTime%1000)/100);
+	// 時刻の表示
+	private void updateCountDownText(){
 
-		int pastminutes;
-		int pastseconds;
-		int pastms;
+		//残り時間
+		int minutes = (int)(leftTime/1000)/60;  //残り時間から分を計算
+		int seconds = (int)(leftTime/1000)%60;  //残り時間から病を計算
+		int ms = (int)((leftTime%1000)/100);    //残り時間から1/10秒を計算
 
-		int timeprogress;
+		//経過時間(モードによって変わるので内容は後述)
+		int pastminutes;    //分
+		int pastseconds;    //秒
+		int pastms;         //ms
 
-		if (mode == 1) { //はぴょう時間なら
-			pastminutes = (int)((ptime-leftTime+100) / 1000)/60;
-			pastseconds = (int)((ptime-leftTime) / 1000)%60;
-			pastms = (int)((ptime-leftTime+100) %1000/100);
-			timeprogress = ((int) (((float) leftTime / (float) ptime) * 100));
+		int timeprogress;   //プログレスバーの割合(0~100)
+
+		//経過時間の処理とプログレスバーの割合計算
+		if (mode == 1) {    //発表時間なら
+			pastminutes = (int)((ptime-leftTime+100) / 1000)/60;                //分
+			pastseconds = (int)((ptime-leftTime) / 1000)%60;                    //秒
+			pastms = (int)((ptime-leftTime+100) %1000/100);                     //ms
+			timeprogress = ((int) (((float) leftTime / (float) ptime) * 100));  //割合(%)
 		} else {
-			pastminutes = (int)((qtime-leftTime+100) / 1000)/60;
-			pastseconds = (int)((qtime-leftTime) / 1000)%60;
-			pastms = (int)((qtime-leftTime+100) %1000/100);
-			timeprogress = ((int) (((float) leftTime / (float) qtime) * 100));
+			pastminutes = (int)((qtime-leftTime+100) / 1000)/60;                //分
+			pastseconds = (int)((qtime-leftTime) / 1000)%60;                    //秒
+			pastms = (int)((qtime-leftTime+100) %1000/100);                     //ms
+			timeprogress = ((int) (((float) leftTime / (float) qtime) * 100));  //割合(%)
 		}
 
-		String timerLeftFormatted = String.format(java.util.Locale.JAPANESE, "%02d:%02d.%01d", minutes, seconds, ms);
-		timerText.setText(timerLeftFormatted);
-		String timerPastFormatted = String.format(java.util.Locale.JAPANESE, "%02d:%02d.%01d", pastminutes, pastseconds, pastms);
-		pastText.setText(timerPastFormatted);
-		timeProgressBar.setProgress(timeprogress);
+		//フォーマット指定
+		String timerLeftFormatted = String.format(java.util.Locale.JAPANESE, "%02d:%02d.%01d", minutes, seconds, ms); //残り
+		String timerPastFormatted = String.format(java.util.Locale.JAPANESE, "%02d:%02d.%01d", pastminutes, pastseconds, pastms); //経過
+
+		timerText.setText(timerLeftFormatted); //残り時間表示
+		pastText.setText(timerPastFormatted);   //経過時間表示
+		timeProgressBar.setProgress(timeprogress);  //プログレスバー更新
 	}
 
+	//点滅
 	public void flash () {
-		if (mode == 1){
-			mode = 3;
-		} else if (mode == 2){
-			mode = 4;
+		if (mode == 1){ //発表モードなら
+			mode = 3; //発表用点滅モードに
+		} else if (mode == 2){  //質問モードなら
+			mode = 4;           //質問用点滅モードに
 		}
 
+		//点滅用にタイマーを作ります(5秒間(またはActivity再生成後の残り時間)、500msごとに更新。)
 		flashCountDown = new CountDownTimer(flashTime,500) {
+			//500ms毎に
 			@Override
 			public void onTick(long millisUntilFinished) {
-				flashTime = millisUntilFinished;
-				flashmode = !flashmode;
-				flashparts();
-				//Log.i("デバッグ", "点滅させています");
+				flashTime = millisUntilFinished;    //残り時間を記録
+				flashmode = !flashmode;             //点滅するので入れ替える(flashparsで使う)
+				flashparts();                       //↑のフラグに従って表示or非表示を切り替える
 			}
 
+			//終わったら
 			@Override
 			public void onFinish() {
-				flashTime = 5000;
-				if (mode == 3) {
+				flashTime = 5000;   //一旦元に戻します
+				if (mode == 3) {    //発表用点滅モードだったら
 					mode = 2; //質問モードに
+
+					//色を赤に
 					start_pause.setBackgroundTintList(ColorStateList.valueOf(getResources().getColor(R.color.tokyu)));
 					timeProgressBar.setBackgroundTintList(ColorStateList.valueOf(getResources().getColor(R.color.tokyu)));
-					startTimer(); //質問時間スタート
-					updateCountDownText();
-				} else if (mode == 4) {
-					resetTimer();
-				}
 
+					startTimer(); //質問時間スタート
+					updateCountDownText(); //おまじない
+				} else if (mode == 4) { //質問用点滅モード
+					resetTimer();   //全部終わったのでリセット
+				}
 
 			}
 		}.start();
 	}
 
+	//点滅させるよ!(表示と非表示入れ替えるよ!)
 	private void flashparts(){
+		//flashmodeは500ms毎に入れ替わってから叩かれるので点滅に見えます
+
 		if (flashmode == true) {
-			timerText.setText("00:00.0");
-			//pastText.setText((((qtime)/1000)/60) + ":00.0");
+			timerText.setText("00:00.0");   //残り0だからね。
+			//pastText.setText((((qtime)/1000)/60) + ":00.0");  //本来はこれやりますがデモは1分未満なので
 			pastText.setText("00.20.0");//デモ用
-			timeProgressBar.setProgress(0);
+			timeProgressBar.setProgress(0); //円が全部青か赤に
 		} else {
-			timerText.setText("");
-			pastText.setText("");
-			timeProgressBar.setProgress(100);
+			timerText.setText("");  //文字が虚無です
+			pastText.setText("");   //こちらも虚無にする
+			timeProgressBar.setProgress(100);   //円を白くする
 		}
 
 	}
 
+	//ラジオボタンを無効化
 	private void radioEnabledFalse(){
-		Log.i("デバッグ", "ラジオボタンを無効化します");
 		for (int i = 0; i < ptime_radiobox.getChildCount(); i++) {
 			ptime_radiobox.getChildAt(i).setEnabled(false);
 		}
@@ -454,6 +483,8 @@ public class MainActivity extends AppCompatActivity {
 		}
 
 	}
+
+	//ラジオボタンを有効化
 	private void radioEnabledTrue(){
 		Log.i("デバッグ", "ラジオボタンを有効化します");
 		for (int i = 0; i < ptime_radiobox.getChildCount(); i++) {
@@ -465,6 +496,8 @@ public class MainActivity extends AppCompatActivity {
 
 	}
 
+
+	/* 以下3つは発表時間をラジオボタンで切り替えてるときに表示部も変えるため */
 	public void radioSetTimeText10(View v){
 		//timerText.setText("10:00.0");
 		timerText.setText("00:20.0"); //デモ用
@@ -484,6 +517,7 @@ public class MainActivity extends AppCompatActivity {
 	protected void onSaveInstanceState(Bundle outState) {
 		Log.i("デバッグ", "状態保存が呼ばれました: "+ ptime);
 		super.onSaveInstanceState(outState);
+
 		//boolean系
 		outState.putBoolean("runstatus", run);
 		outState.putBoolean("finishedstatus", finished);
